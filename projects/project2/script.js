c1f88c85a62147f6d0de13176dfdfe8ecf97ce1f
sarahzupnik <szupnik@pdx.edu>
2020-12-01 09:19:03 -0800

    updated desktop code for homepage

--- projects/project2/script.js
+++ projects/project2/script.js
@@ -77,23 +77,6 @@ $('a[href*="#"]')
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var nav = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky")
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
-
-//end sticky nav
-
 //quantity buttons
 
 var input = document.getElementById('theInput');
