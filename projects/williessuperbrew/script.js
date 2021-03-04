db852997520604c87e1d6d189770a32a10bcf70d
alannarisse <angrypirate@gmail.com>
2021-03-04 12:21:56 -0800

    fixed scroll magic and other things

--- projects/williessuperbrew/script.js
+++ projects/williessuperbrew/script.js
@@ -1,58 +1,50 @@
-'use strict'
+"use strict"
 
 function toggleIcon() {
-    $('.icon').on('click', function(){
-        $('.icon').toggleClass('active');
-        $('.menu-mobile').slideToggle(300);
-    })
+  $(".icon").on("click", function () {
+    $(".icon").toggleClass("active")
+    $(".menu-mobile").slideToggle(300)
+  })
 }
 
-function closeMobileMenu(){
-    $('.menu-mobile').on('click', 'a', function(){
-        $('.icon').trigger('click');
-    })
+function closeMobileMenu() {
+  $(".menu-mobile").on("click", "a", function () {
+    $(".icon").trigger("click")
+  })
 }
-
-
 
 function desktopMenu() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 0) {
-            $('header.desktop').fadeIn(500);
-        } else {
-            $('header.desktop').fadeOut(500);
-        }
-    })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 0) {
+      $("header.desktop").fadeIn(500)
+    } else {
+      $("header.desktop").fadeOut(500)
+    }
+  })
 }
-
-//when the page loads call toggleIcon;
-$(toggleIcon);
-$(closeMobileMenu);
-$(desktopMenu);
 
 //dropdown menu
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show")
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content")
+    var i
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show")
       }
     }
   }
+}
 
-  //END dropdown menu
-
+//END dropdown menu
 
 //scrolling for menu
 
@@ -61,66 +53,91 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      var target = $(this.hash)
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]")
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+        event.preventDefault()
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target)
+            $target.focus()
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false
+            } else {
+              $target.attr("tabindex", "-1") // Adding tabindex for elements not focusable
+              $target.focus() // Set focus again
+            }
+          }
+        )
       }
     }
-  });
+  })
 
 //endauto scroll
 
 //start can menus
 
-$("#lime-toggle").click(function() {
-  $("#lime-menu").fadeToggle(300);
-});
+$("#lime-toggle").click(function () {
+  $("#lime-menu").fadeToggle(300)
+})
 
-$("#blue-toggle").click(function() {
-  $("#blue-menu").fadeToggle(300);
-});
+$("#blue-toggle").click(function () {
+  $("#blue-menu").fadeToggle(300)
+})
 
-$("#mango-toggle").click(function() {
-  $("#mango-menu").fadeToggle(300);
-});
+$("#mango-toggle").click(function () {
+  $("#mango-menu").fadeToggle(300)
+})
 
-$("#pom-toggle").click(function() {
-  $("#pom-menu").fadeToggle(300);
-});
+$("#pom-toggle").click(function () {
+  $("#pom-menu").fadeToggle(300)
+})
 
+// box closes itself
+$("#lime-menu").click(function () {
+  $("#lime-menu").fadeToggle(300)
+})
+$("#blue-menu").click(function () {
+  $("#blue-menu").fadeToggle(300)
+})
+$("#mango-menu").click(function () {
+  $("#mango-menu").fadeToggle(300)
+})
+$("#pom-menu").click(function () {
+  $("#pom-menu").fadeToggle(300)
+})
 //end can menus
 
 //start scroll magic
-
-
-var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({
-  triggerElement:'.cans'
-})
-.setClassToggle('.can-group','slide-in')
-.addTo(controller);
+function canScroll() {
+  var controller = new ScrollMagic.Controller()
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".can-group"
+  })
+    .setClassToggle(".can-group", "slide-in-top")
+    // .addIndicators({
+    //   name: "fade scene",
+    //   colorTrigger: "black",
+    //   indent: 200,
+    //   colorStart: "#75C695"
+    // }) // this requires a plugin
+    .addTo(controller)
+}
+//when the page loads call toggleIcon;
+$(toggleIcon)
+$(closeMobileMenu)
+$(desktopMenu)
+$(canScroll)
